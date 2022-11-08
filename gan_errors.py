bd4f9c34f63369cd5ed5b05eac66255a2c18c440
ajigeo <46032638+ajigeo@users.noreply.github.com>
2022-11-08 15:42:17 +0530

    Update gan_errors.py

--- gan_errors.py
+++ gan_errors.py
@@ -1,10 +1,6 @@
-import sewar
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 import numpy as np
-from matplotlib import pyplot as plt
 #import seaborn as sns
-import statsmodels.api as sm
-from statsmodels.stats.outliers_influence import summary_table
 from osgeo import gdal
 def read_image(filename):
     image = gdal.Open(filename)
@@ -13,41 +9,189 @@ def read_image(filename):
     image_array = image.ReadAsArray()
     #image_shape = image_array.shape()
     return image_array
-
+#%%
 #x = np.linspace(0,10)
 #y = 3*np.random.randn(50) + x
 #X = sm.add_constant(x)
 #res = sm.OLS(y, X).fit()
-x = read_image(r'E:\data\ISRO-project\intermediate_products\VH_predicted_NDVI\act_06.tif')
+x = read_image(r'E:\data\ISRO-project\intermediate_products\VV_predicted_NDVI\act_01.tif')
 x = np.asarray(x).ravel()
-y = read_image(r'E:\data\ISRO-project\intermediate_products\VH_predicted_NDVI\pred_06.tif')
-y = np.asarray(y).ravel()
-X = sm.add_constant(x)
-res = sm.OLS(y, X).fit()
 
-st, data, ss2 = summary_table(res, alpha=0.05)
-fittedvalues = data[:,2]
-predict_mean_se  = data[:,3]
-predict_mean_ci_low, predict_mean_ci_upp = data[:,4:6].T
-predict_ci_low, predict_ci_upp = data[:,6:8].T
-
-# https://stackoverflow.com/questions/51917377/different-results-of-seaborn-and-statsmodels
-
-plt.figure(1)
-plt.plot(x, y, 'o', label="data")
-plt.plot(X[:,1], fittedvalues, 'r-', label='OLS')
-plt.plot(X[:,1], predict_ci_low, 'b--')
-plt.plot(X[:,1], predict_ci_upp, 'b--')
-plt.plot(X[:,1], predict_mean_ci_low, 'g--')
-plt.plot(X[:,1], predict_mean_ci_upp, 'g--')
-plt.legend()
-
-print("mean is " + str(np.mean(y)))
-print("SD is " + str(np.std(y)))
-print("95% CI is " + str((1.96*(np.std(y)/np.sqrt(len(y))))))
-print("CI is " + str(1.96 * np.sqrt((0.145 * (1 - 0.145)) / 65536)))
-print("Correlation is " + str(np.corrcoef(x,y)[0][1]))
+vv_y = read_image(r'E:\data\ISRO-project\intermediate_products\VV_predicted_NDVI\pred_01.tif')
+vv_y = np.asarray(vv_y).ravel()
 
 
-mse = mean_squared_error(x,y,squared=False)
-mae = mean_absolute_error(x,y)
+#vh_y = read_image(r'E:\data\ISRO-project\intermediate_products\VH_predicted_NDVI\pred_12.tif')
+#vh_y = np.asarray(vh_y).ravel()
+
+
+# Prediction interval
+# https://machinelearningmastery.com/prediction-intervals-for-machine-learning/
+from numpy import sum as arraysum
+def compute_errors(x,y):
+    sum_errs = arraysum((x - y)**2)
+    stdev = np.sqrt(1/(len(y)-2) * sum_errs)
+    p_interval = 1.96 * stdev #95% 
+    
+    
+    # Confidence interval
+    # https://machinelearningmastery.com/confidence-intervals-for-machine-learning/
+    mse = mean_squared_error(x,y,squared=False)
+    mae = mean_absolute_error(x,y)
+    mse_interval = 1.96 * np.sqrt((mse * (1 - mse)) / len(y))
+    mae_interval = 1.96 * np.sqrt((mae * (1 - mse)) / len(y))
+    
+    print("Correlation is " + str(np.corrcoef(x,y)[0][1]))
+    print("The MSE of the model @ 95% confidence interval is " + str(mse) + " ± "+ str(mse_interval))
+    print("The MAE of the model @ 95% confidence interval is " + str(mae) + " ± "+ str(mae_interval))
+    print("Theres a 95% likelihood that the true value is between +/- " +str(p_interval))
+
+compute_errors(x,vv_y)    
+'''
+A confidence interval is different from a tolerance interval that describes the bounds of data sampled from the distribution. 
+It is also different from a prediction interval that describes the bounds on a single observation. 
+Instead, the confidence interval provides bounds on a population parameter, such as a mean, standard deviation, or similar.
+'''
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+ 
+# VH errors
+x = np.arange(1,13)
+y1_vh = np.array([0.4124,0.84173,0.51484,0.10983,0.40876,0.41284,0.52695,0.40809,0.37002,0.41266,0.49665,0.51216])
+y2_vh = np.array([0.62522,0.76512,0.51763,0.19303,0.43448,0.44742,0.65637,0.46985,0.6604,0.72288,0.44539,0.46113])
+y3_vh = np.array([0.58891,	0.55369,0.62717,0.3358,0.48019,	0.55051,	0.6394,	0.54458,	0.59896,	0.70515,	0.75597,	0.6656])
+y4_vh = np.array([0.60932,	0.66379,0.43503,	0.27346,	0.45331,	0.45779,	0.60929,	0.46077,	0.43335,	0.5885,	0.41007,	0.47045])
+y5_vh = np.array([0.56124,	0.59359,0.518,	0.26847,	0.38512,	0.45205,	0.59728,	0.48876,	0.49527,	0.4574,	0.58739,	0.51079])
+y6_vh = np.array([0.61454,	0.86809,0.59527,	0.49525,	0.56569,	0.5143,	0.5901,	0.51366,	0.56048,	0.79072,	0.47892,	0.52486])
+y7_vh = np.array([0.5689,	0.75806,0.49041,	0.16188,	0.39729,	0.47352,	0.59303,	0.50696,	0.56255,	0.73363,	0.42894,	0.51859])
+y8_vh = np.array([0.57831,	0.51219,	0.52674,	0.12989,	0.37571,	0.47253,	0.57446,	0.45843,	0.55072,	0.64931,	0.48344,	0.56328])
+y9_vh = np.array([0.55225,	0.65951,	0.51754,	0.21619,	0.4255,	0.43788,	0.60648,	0.44265,	0.60525,	0.42929,	0.73496,	0.51965])
+y10_vh = np.array([0.58942,	0.59707,	0.4993,	0.1971,	0.39696,	0.41901,	0.57021,	0.55934,	0.50935,	0.44468,	0.66754,	0.51132])
+# defining our error
+y_error_vh = np.array([0.384,0.302,0.208,0.347,0.405,0.284,0.518,0.398,0.550,0.368,0.486,0.760])
+#%%
+# VH errors
+
+y1_vv = np.array([0.65692,	0.73402,	0.6857,	0.65899,	0.65577,	0.6605,	0.62021,	0.72125,	0.47783,	0.65139,	0.77855,	0.70135])
+y2_vv = np.array([0.69762,	0.66768,	0.58878,	0.59835,	0.60348,	0.56343,	0.57429,	0.69304,	0.70755,	0.585,	0.51674,	0.5913])
+y3_vv = np.array([0.69978,	0.59619,	0.63285,	0.74417,	0.73386,	0.71239,	0.70586,	0.71532,	0.61353,	0.68795,	0.85108,	0.61352])
+y4_vv = np.array([0.65868,	0.6048,	0.71262,	0.60371,	0.71964,	0.70503,	0.62094,	0.73199,	0.65343,	0.64868,	0.50163,	0.5908])
+y5_vv = np.array([0.70514,	0.64873,	0.64815,	0.63292,	0.61018,	0.60604,	0.70288,	0.65666,	0.61891,	0.48841,	0.66838,	0.61837])
+y6_vv = np.array([0.72959,	0.65365,	0.70385,	0.72908,	0.65124,	0.65289,	0.66343,	0.73633,	0.65103,	0.64104,	0.50608,	0.56784])
+y7_vv = np.array([0.57323,	0.7461,	0.59292,	0.62223,	0.60231,	0.57734,	0.66071,	0.75028,	0.6711,	0.72777,	0.75792,	0.64571])
+y8_vv = np.array([0.7155,	0.62818,	0.56612,	0.65733,	0.60036,	0.6133,	0.63963,	0.6039,	0.693,	0.57926,	0.58323,	0.71554])
+y9_vv = np.array([0.671,	0.71379,	0.64881,	0.71357,	0.59384,	0.62424,	0.64405,	0.59428,	0.62293,	0.53812,	0.6752,	0.64174])
+y10_vv = np.array([0.72792,	0.61915,	0.6356,	0.72627,	0.64554,	0.584,	0.63748,	0.59899,	0.61026,	0.52238,	0.71007,	0.69491])
+# defining our error
+y_error_vv = np.array([0.590544817205121,	0.302588879835678,	0.370155694783363,	0.586432848457724,	0.655974279381866,	0.45308466738304,	0.498016916792174,	0.612729455258196,	0.667873079137876,	0.353962672231628,	0.592107856103136,	0.909909866711467])
+#%%
+# https://www.geeksforgeeks.org/errorbar-graph-in-python-using-matplotlib/
+l = 3
+b = 4
+plt.subplot(l,b,1)
+plt.plot(x, y1_vh)
+plt.errorbar(x, y1_vh, yerr = y_error_vh, fmt ='o',color='black') 
+plt.title('Field 1')
+
+plt.subplot(l,b,2)
+plt.plot(x, y2_vh)
+plt.errorbar(x, y2_vh, yerr = y_error_vh, fmt ='o',color='black') 
+plt.title('Field 2')
+
+plt.subplot(l,b,3)
+plt.plot(x, y3_vh)
+plt.errorbar(x, y3_vh, yerr = y_error_vh, fmt ='o',color='black') 
+plt.title('Field 3')
+
+plt.subplot(l,b,4)
+plt.plot(x, y4_vh)
+plt.errorbar(x, y4_vh, yerr = y_error_vh, fmt ='o',color='black') 
+plt.title('Field 4')
+
+plt.subplot(l,b,5)
+plt.plot(x, y5_vh)
+plt.errorbar(x, y5_vh, yerr = y_error_vh, fmt ='o',color='black') 
+plt.title('Field 5')
+
+plt.subplot(l,b,6)
+plt.plot(x, y6_vh)
+plt.errorbar(x, y6_vh, yerr = y_error_vh, fmt ='o',color='black') 
+plt.title('Field 6')
+
+plt.subplot(l,b,7)
+plt.plot(x, y7_vh)
+plt.errorbar(x, y7_vh, yerr = y_error_vh, fmt ='o',color='black') 
+plt.title('Field 7')
+
+plt.subplot(l,b,8)
+plt.plot(x, y8_vh)
+plt.errorbar(x, y8_vh, yerr = y_error_vh, fmt ='o',color='black') 
+plt.title('Field 8')
+
+plt.subplot(l,b,10)
+plt.plot(x, y9_vh)
+plt.errorbar(x, y9_vh, yerr = y_error_vh, fmt ='o',color='black') 
+plt.title('Field 9')
+
+plt.subplot(l,b,11)
+plt.plot(x, y10_vh)
+plt.errorbar(x, y10_vh, yerr = y_error_vh, fmt ='o',color='black') 
+plt.title('Field 10')
+
+plt.show()
+#%%
+l = 3
+b = 4
+plt.subplot(l,b,1)
+plt.plot(x, y1_vv)
+plt.errorbar(x, y1_vv, yerr = y_error_vv, fmt ='o',color='black') 
+plt.title('Field 1')
+
+plt.subplot(l,b,2)
+plt.plot(x, y2_vv)
+plt.errorbar(x, y2_vv, yerr = y_error_vv, fmt ='o',color='black') 
+plt.title('Field 2')
+
+plt.subplot(l,b,3)
+plt.plot(x, y3_vv)
+plt.errorbar(x, y3_vv, yerr = y_error_vv, fmt ='o',color='black') 
+plt.title('Field 3')
+
+plt.subplot(l,b,4)
+plt.plot(x, y4_vv)
+plt.errorbar(x, y4_vv, yerr = y_error_vv, fmt ='o',color='black') 
+plt.title('Field 4')
+
+plt.subplot(l,b,5)
+plt.plot(x, y5_vv)
+plt.errorbar(x, y5_vv, yerr = y_error_vv, fmt ='o',color='black') 
+plt.title('Field 5')
+
+plt.subplot(l,b,6)
+plt.plot(x, y6_vv)
+plt.errorbar(x, y6_vv, yerr = y_error_vv, fmt ='o',color='black') 
+plt.title('Field 6')
+
+plt.subplot(l,b,7)
+plt.plot(x, y7_vv)
+plt.errorbar(x, y7_vv, yerr = y_error_vv, fmt ='o',color='black') 
+plt.title('Field 7')
+
+plt.subplot(l,b,8)
+plt.plot(x, y8_vv)
+plt.errorbar(x, y8_vv, yerr = y_error_vv, fmt ='o',color='black') 
+plt.title('Field 8')
+
+plt.subplot(l,b,10)
+plt.plot(x, y9_vv)
+plt.errorbar(x, y9_vv, yerr = y_error_vv, fmt ='o',color='black') 
+plt.title('Field 9')
+
+plt.subplot(l,b,11)
+plt.plot(x, y10_vv)
+plt.errorbar(x, y10_vv, yerr = y_error_vv, fmt ='o',color='black') 
+plt.title('Field 10')
+
+plt.show()
